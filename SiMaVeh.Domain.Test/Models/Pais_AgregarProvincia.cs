c68c514e3163a5f15a9e70447073effe09177610
Alex Humar <alexhumar@outlook.com>
2020-05-10 10:34:24 -0300

    Implementacion completa de tests sobre agregar/quitar Provincias y cambiar Pais

--- SiMaVeh.Domain.Test/Models/Pais_AgregarProvincia.cs
+++ SiMaVeh.Domain.Test/Models/Pais_AgregarProvincia.cs
@@ -7,11 +7,13 @@ namespace SiMaVeh.Domain.Test.Models
     public class Pais_AgregarProvincia
     {
         private Pais _argentina;
+        private Pais _uruguay;
 
         [SetUp]
         public void Setup()
         {
             _argentina = new Pais { Id = 1, Nombre = NombrePais.Argentina };
+            _uruguay = new Pais { Id = 2, Nombre = NombrePais.Uruguay };
         }
 
         [Test]
@@ -25,6 +27,27 @@ namespace SiMaVeh.Domain.Test.Models
 
             Assert.That(_argentina.Provincias, Contains.Item(_buenosAires));
             Assert.AreSame(_buenosAires.Pais, _argentina);
+        }
+
+        [Test]
+        public void AgregarProvincia_BsAsAArgentina_RetornaPaisCambiadoConProvinciaAgregada()
+        {
+            Assert.IsEmpty(_argentina.Provincias);
+            Assert.IsEmpty(_uruguay.Provincias);
+
+            var _buenosAires = new Provincia { Id = 1, Nombre = NombreProvincia.BuenosAires };
+
+            _uruguay.Agregar(_buenosAires);
+
+            Assert.That(_uruguay.Provincias, Contains.Item(_buenosAires));
+            Assert.AreSame(_buenosAires.Pais, _uruguay);
+            Assert.IsEmpty(_argentina.Provincias);
+
+            _argentina.Agregar(_buenosAires);
+
+            Assert.That(_argentina.Provincias, Contains.Item(_buenosAires));
+            Assert.AreSame(_buenosAires.Pais, _argentina);
+            Assert.IsEmpty(_uruguay.Provincias);
         }
     }
 }
