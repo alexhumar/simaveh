2b3a75c776f9f637dc0b3326eacabe867dfdb3c4
Alex Humar <alexhumar@outlook.com>
2020-05-13 22:41:00 -0300

    Agregado partidos catamarca. Documentacion partidos bs as.

---
+++ SiMaVeh.Domain/DataSeed/Constants/NombrePartidoCatamarca.cs
@@ -0,0 +1,88 @@
+﻿namespace SiMaVeh.Domain.DataSeed.Constants
+{
+    /// <summary>
+    /// Constante de nombres de Partidos de Catamarca
+    /// </summary>
+    public class NombrePartidoCatamarca
+    {
+        /// <summary>
+        /// Ambato
+        /// </summary>
+        public const string Ambato = "Ambato";
+
+        /// <summary>
+        /// Ancasti
+        /// </summary>
+        public const string Ancasti = "Ancasti";
+
+        /// <summary>
+        /// Andalgalá
+        /// </summary>
+        public const string Andalgal = "Andalgalá";
+
+        /// <summary>
+        /// Antofagasta de la Sierra
+        /// </summary>
+        public const string AntofagastaDeLaSierra = "Antofagasta de la Sierra";
+
+        /// <summary>
+        /// Belén
+        /// </summary>
+        public const string Belen = "Belén";
+
+        /// <summary>
+        /// Capayán
+        /// </summary>
+        public const string Capayan = "Capayán";
+
+        /// <summary>
+        /// Capital
+        /// </summary>
+        public const string Capital = "Capital";
+
+        /// <summary>
+        /// El Alto
+        /// </summary>
+        public const string ElAlto = "El Alto";
+
+        /// <summary>
+        /// Fray Mamerto Esquiú
+        /// </summary>
+        public const string FrayMamertoEsquiu = "Fray Mamerto Esquiú";
+
+        /// <summary>
+        /// La Paz
+        /// </summary>
+        public const string LaPaz = "La Paz";
+
+        /// <summary>
+        /// Paclín
+        /// </summary>
+        public const string Paclin = "Paclín";
+
+        /// <summary>
+        /// Pomán
+        /// </summary>
+        public const string Poman = "Pomán";
+
+        /// <summary>
+        /// Santa María
+        /// </summary>
+        public const string SantaMaria = "Santa María";
+
+        /// <summary>
+        /// Santa Rosa
+        /// </summary>
+        public const string SantaRosa = "Santa Rosa";
+
+        /// <summary>
+        /// Tinogasta
+        /// </summary>
+        public const string Tinogasta = "Tinogasta";
+
+        /// <summary>
+        /// Valle Viejo
+        /// </summary>
+        public const string ValleViejo = "Valle Viejo";
+    }
+}
