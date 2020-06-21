1b454e10d0abe552cb5c07efdf06e60bca21a2ce
Alex Humar <alexhumar@yahoo.com>
2020-06-21 19:01:29 -0300

    Etapa inicial dataseed finalizada.

--- SiMaVeh.DataAcces/Migrations/20200621215722_InitialDataSeed.Designer.cs
+++ SiMaVeh.DataAcces/Migrations/20200621215722_InitialDataSeed.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SiMaVeh.DataAccess.Model;
 
 namespace SiMaVeh.DataAccess.Migrations
 {
     [DbContext(typeof(SiMaVehContext))]
-    partial class SiMaVehContextModelSnapshot : ModelSnapshot
+    [Migration("20200621215722_InitialDataSeed")]
+    partial class InitialDataSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
