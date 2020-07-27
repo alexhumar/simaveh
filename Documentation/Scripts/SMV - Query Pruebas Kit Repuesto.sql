eebc9a03757f619b9ec4c1795fb08a65eef48f5e
Alex Humar <alexhumar@yahoo.com>
2020-07-27 19:38:37 -0300

    Actualizacion de scripts de prueba

--- Documentation/Scripts/SMV - Query Pruebas Kit Repuesto.sql
+++ Documentation/Scripts/SMV - Query Pruebas Kit Repuesto.sql
@@ -3,8 +3,10 @@ INSERT INTO modelosvehiculo (Id, GrupoModeloId, Version, TipoFuenteEnergiaId) VA
 INSERT INTO modelosvehiculo (Id, GrupoModeloId, Version, TipoFuenteEnergiaId) VALUES (2, 1, "XS", 6);
 INSERT INTO targetsmantenimiento (Id, Nombre, Descripcion, Tipo) VALUES (1, "Parrilla", "Parrilla de suspension", "P");
 INSERT INTO targetsmantenimiento (Id, Nombre, Descripcion, Tipo) VALUES (2, "Radiador", "Radiador del circuito de refrigeracion", "P");
-INSERT INTO recambios (Id, MarcaId, CodigoIdentificador, Tipo, TargetMantenimientoId, ModeloVehiculoId) VALUES (1, 357, "PARRVALEO123", "R", 1,  1);
-INSERT INTO recambios (Id, MarcaId, CodigoIdentificador, Tipo, TargetMantenimientoId, ModeloVehiculoId) VALUES (2, 357, "RADVALEO123", "R", 2,  1);
+INSERT INTO recambios (Id, MarcaId, CodigoIdentificador, Tipo, TargetMantenimientoId) VALUES (1, 357, "PARRVALEO123", "R", 1);
+INSERT INTO recambios (Id, MarcaId, CodigoIdentificador, Tipo, TargetMantenimientoId) VALUES (2, 357, "RADVALEO123", "R", 2);
 INSERT INTO recambios (Id, MarcaId, Tipo, Nombre, Descripcion) VALUES (3, 357, "K", "Kit radiador + parrilla valeo", "Kit radiador + parrilla valeo");
 INSERT INTO kitrepuesto (KitId, RepuestoId) VALUES (3, 1);
 INSERT INTO kitrepuesto (KitId, RepuestoId) VALUES (3, 2);
+INSERT INTO modelovehiculorepuesto (ModeloVehiculoId, RepuestoId) VALUES (1, 1);
+INSERT INTO modelovehiculorepuesto (ModeloVehiculoId, RepuestoId) VALUES (1, 2);
