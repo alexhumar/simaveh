4f0c6f58e65985588b1b9ceb4d69ac9308fb213a
Alex Humar <alexhumar@hotmail.com>
2020-11-13 17:25:34 -0300

    Correccion en representacion Jerarquia TPT

--- SiMaVeh.DataAcces/Model/ConfiguradoresContext/ConfiguradorJerarquiaTPT.cs
+++ SiMaVeh.DataAcces/Model/ConfiguradoresContext/ConfiguradorJerarquiaTPT.cs
@@ -8,12 +8,16 @@ namespace SiMaVeh.DataAccess.Model.ConfiguradoresContext
     {
         public void Configurar(ModelBuilder builder)
         {
+            builder.Entity<Vehiculo>().ToTable("Vehiculos");
             builder.Entity<Automovil>().ToTable("Automoviles");
+            builder.Entity<TargetMantenimiento>().ToTable("TargetsMantenimiento");
             builder.Entity<Pieza>().ToTable("Piezas");
             builder.Entity<Fluido>().ToTable("Fluidos");
             builder.Entity<Aceite>().ToTable("Aceites");
+            builder.Entity<Persona>().ToTable("Personas");
             builder.Entity<Reparador>().ToTable("Reparadores");
             builder.Entity<Usuario>().ToTable("Usuarios");
+            builder.Entity<Recambio>().ToTable("Recambios");
             builder.Entity<Kit>().ToTable("Kits");
             builder.Entity<Repuesto>().ToTable("Repuestos");
         }
